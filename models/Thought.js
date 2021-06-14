700cf18084e65cdcaab0da0f8eff34f44b91bac4
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-06-14 16:02:12 -0500

    reactions schema fixed and moved to Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,30 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema({
+  reactionId: [
+    {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+  ],
+  reactionBody: {
+    type: String,
+    required: "Text is Required",
+    minLength: 1,
+    maxLength: 280,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal),
+  },
+  username: {
+    type: String,
+    required: "Username is Required",
+  },
+});
 
 const ThoughtSchema = new Schema(
   {
